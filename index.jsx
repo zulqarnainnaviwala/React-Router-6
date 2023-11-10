808af08452380e7442ab8ba6ac9db3a02972fae4
zulqarnainnaviwala <zulqarnainnaviwala@gmail.com>
2023-11-11 04:09:46 +0500

    Van & VanDetails, Routes, Created Miragejs Server

--- index.jsx
+++ index.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
+
+import "./server"
 
 function App() {
   return (
@@ -11,11 +15,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   )
@@ -24,3 +31,10 @@ function App() {
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(<App />);
+  
+      // <Routes>
+      //   <Route path="/" element={<Home />} />
+      //   <Route path="/about" element={<About />} />
+      //   <Route path="/vans" element={<Vans />} />
+      //   <Route path="/vans/:id" element={<VanDetail />} />
+      // </Routes>
